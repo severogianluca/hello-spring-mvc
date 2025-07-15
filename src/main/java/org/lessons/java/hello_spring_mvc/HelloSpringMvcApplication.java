efda86afe347ae046bce237c4b90a7599050b9ff
severogianluca <gianlucasevero300@gmail.com>
2025-07-15 05:10:22 +0200

    first commit

---
+++ src/main/java/org/lessons/java/hello_spring_mvc/HelloSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.hello_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/hello_spring_mvc/HelloSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.hello_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
