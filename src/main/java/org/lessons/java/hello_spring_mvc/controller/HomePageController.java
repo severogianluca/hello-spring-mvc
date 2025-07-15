bf4bcde422382e5cf69038abaa71f520cdb2b9de
severogianluca <gianlucasevero300@gmail.com>
2025-07-15 17:58:06 +0200

    add: anagrafe page

--- src/main/java/org/lessons/java/hello_spring_mvc/controller/HomePageController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controller/HomePageController.java
@@ -1,6 +1,9 @@
 package org.lessons.java.hello_spring_mvc.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,4 +18,16 @@ public class HomePageController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/anagrafe")
+    public String anagrafe(@RequestParam String name, Model model) {
+
+        model.addAttribute("cognome", "severo");
+        model.addAttribute("codiceFiscale", "SVRGLC94C23H501I");
+        model.addAttribute("currentDate", LocalDateTime.now());
+        model.addAttribute("name", name);
+
+        return "anagrafe";
+    }
+    
 }
